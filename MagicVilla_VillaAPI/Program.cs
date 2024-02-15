a753a9b99c032feeff183971ad90f8e1ea3a92c7
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-02-16 00:27:05 +0300

    Logger added.

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,10 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(option =>
-{
-    option.ReturnHttpNotAcceptable = true;
+
+
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/villagLogs.txt",
+//    rollingInterval:RollingInterval.Day).CreateLogger();
+//builder.Host.UseSerilog();
+
+builder.Services.AddControllers(option =>{
+    //option.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
